1ac29b5a1f11da0b78bfcb301ec0ea76dc9bdeb7
daniel-onyenwee <danielonyenwee@gmail.com>
2024-06-14 01:18:12 +0100

    ➕ add cli argument -> models_dir to parser_detect and parser_recognize subparsers

--- cli/cli_parser.py
+++ cli/cli_parser.py
@@ -4,10 +4,12 @@ parser = argparse.ArgumentParser(prog="PyJsFaceApi", description="A CLI tool for
 
 subparsers = parser.add_subparsers(dest="command", help="Sub-commands: 'detect' and 'recognize'")
 
- # Sub-parser for face detection
-parser_detect  = subparsers.add_parser("detect", help="Detect faces in an image.")
+# Sub-parser for face detection
+parser_detect = subparsers.add_parser("detect", help="Detect faces in an image.")
+parser_detect.add_argument("models_dir", type=str, help="Face recognition models location.")
 parser_detect.add_argument("-p", "--pretty", action="store_false", help="Pretty print the CLI result.")
 
 # Sub-parser for face recognition
 parser_recognize = subparsers.add_parser("recognize", help="Recognize faces in an image based on known faces.")
+parser_recognize.add_argument("models_dir", type=str, help="Face recognition models location.")
 parser_recognize.add_argument("-p", "--pretty", action="store_false", help="Pretty print the CLI result.")
