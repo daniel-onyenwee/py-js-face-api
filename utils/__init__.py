de1418c91365fea74a2a4e674b0cfbfa6a6d72af
daniel-onyenwee <danielonyenwee@gmail.com>
2024-06-14 01:35:01 +0100

    ➕ add utils -> face_recognition

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
-from .base64_to_image import *
-from .image_to_base64 import *
-from .is_valid_base64 import *
+from .base64_to_image import base64_to_image
+from .image_to_base64 import image_to_base64
+from .is_valid_base64 import is_valid_base64
+import utils.face_recognition as face_recognition
